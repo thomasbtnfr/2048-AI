579872ab9f164f0bbde2921d056b9041c03671e2
tbetton <thomas.betton@insa-rennes.fr>
2021-03-17 20:17:19 +0100

    evaluate model + score interpretation

--- game_loop.py
+++ game_loop.py
@@ -4,6 +4,8 @@ from minimax import getBestMove
 import time
 import sys, os
 import numpy as np
+import pandas as pd
+from collections import Counter
 
 def startRemoteController():
     gameDriver = GameDriver()
@@ -47,9 +49,49 @@ def startTerminal():
         os.system('cls' if os.name=='nt' else 'clear')
         print(f'Move #{moves_count}: {moves_str[moveCode[0]]} | Utility {round(moveCode[1],2)} | Score {grid.score} | NbEmpty {grid.nbEmpty()}')
         moves_count += 1
+    
+    return grid.score, grid.maxValue()
 
-startTerminal()
+
+def writeResultat(fileName: str, maxTile: int, score: int):
+    resultats = []
+    resultats.append(str(maxTile) + ';' + str(score) + '\n')
+    with open(fileName + '.txt', 'a') as f:
+        f.write(''.join(resultats))
+
+
+def evaluateModel(NbGame: int, modelName: str):
+    for i in range(NbGame):
+        score, maxTile = startTerminal()
+        writeResultat(modelName, maxTile, score)
+
+
+def readResFile(fileName: str):
+    colnames = ['MaxTile','Score']
+    res = pd.read_csv(fileName + '.txt',names = colnames, sep=';')
+    print('-'*100)
+    print('Results algorithm :', fileName, '| Number of games : ', res.shape[0])
+    print('-'*100)
+    maxTiles = res['MaxTile'].tolist()
+    scores = res['Score'].tolist()
+    print('Max Tile reached : ', res['MaxTile'].max())
+    print('Maximum Tile Mean : ', res['MaxTile'].mean())
+    print('-'*100)
+    print('Score Max reached : ', res['Score'].max())
+    print('Score Mean : ', res['Score'].mean())
+    print('-'*100)
+    
+    print('Probability to reach the following values')
+    counter = Counter(maxTiles)
+    for key in counter:
+        probability = counter[key] / len(maxTiles) * 100
+        print(key, '|', str(probability) + '%')
+        
+
+#evaluateModel(5, 'minmax')
+#startTerminal()
 #startRemoteController()
+readResFile('minmax')
 
 """
 grid = Grid([[]])
